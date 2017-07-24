7c9246202e1122d8021ea973f592e6538baed887
inon19-meet <inon19@meet.mit.edu>
2017-07-24 18:31:38 +0300

    Stopping here

--- MEETinTurtle.py
+++ MEETinTurtle.py
@@ -41,38 +41,71 @@ turtle.penup()
 
 #Draw the E:
 
-turtle.goto(0,-100+200)
+turtle.goto(-50,-100+200)
 
 turtle.pendown() #Put the pen down to start
 
-turtle.goto(0,-100)
+turtle.goto(-50,-100)
 
-turtle.goto(0,-100+200)
+turtle.goto(-50,-100+200)
 
-turtle.goto(0,-100)
+turtle.goto(-50,-100)
 
-turtle.goto(100+50,-100)
+turtle.goto(50+50,-100)
 
 turtle.penup()
 
-turtle.goto(0,-100)
+turtle.goto(-50,-100)
 
-turtle.goto(0,-100+100)
+turtle.goto(-50,-100+100)
 
 turtle.pendown()
 
-turtle.goto(150,0)
+turtle.goto(100,0)
 
+turtle.penup()
 
+turtle.goto(100,100)
 
+turtle.pendown()
 
+turtle.goto(100-150,100)
 
+turtle.penup() #Pick up the pen so it doesn’t
 
+#Draw the E2:
 
+turtle.goto(150,-100+200)
 
+turtle.pendown() #Put the pen down to start
 
+turtle.goto(150,-100)
 
+turtle.goto(150,-100+200)
 
+turtle.goto(150,-100)
+
+turtle.goto(150+150,-100)
+
+turtle.penup()
+
+turtle.goto(150,-100)
+
+turtle.goto(150,-100+100)
+
+turtle.pendown()
+
+turtle.goto(150+150,0)
+
+turtle.penup()
+
+turtle.goto(150,100)
+
+turtle.pendown()
+
+turtle.goto(150+150,100)
+
+turtle.penup() #Pick up the pen so it doesn’t
 
 
 
